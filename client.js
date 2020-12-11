dd5797b92bd02b716b67c56364f5857af7900d5e
Travis Huss <travisjhuss@gmail.com>
2020-12-11 15:02:48 -0600

    created enterEmployee function to run on click of submit button. no logic yet but connected and working

--- client.js
+++ client.js
@@ -2,4 +2,10 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log('JQ Loaded');
+    $('#submitButton').on('click', enterEmployee);
 } // end handleReady
+
+function enterEmployee() {
+    console.log('clicked');
+    
+} // end enterEmployee
