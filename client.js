c834ad486f956c2ad1123b3c00abb3e62bc0f2e7
Travis Huss <travisjhuss@gmail.com>
2020-12-11 15:15:46 -0600

    created enterEmployee function and set it up to create newEmployee object. tested

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 $(document).ready(handleReady);
 
+let employees = [];
+
 function handleReady() {
     console.log('JQ Loaded');
     $('#submitButton').on('click', enterEmployee);
@@ -7,5 +9,23 @@ function handleReady() {
 
 function enterEmployee() {
     console.log('clicked');
+    // create employee object
+    let newEmployee = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        id: $('#idIn').val(),
+        title: $('#titleIn').val(),
+        annualSalary: $('#salaryIn').val()
+    }
+
+    console.log(newEmployee);
     
+    // push into employee array
+
+    // render to DOM
+
+    // clear inputs
+
+    // calculate totalMonthlySalaries
+
 } // end enterEmployee
